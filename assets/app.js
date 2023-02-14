c756642deadfb761883e422b572a672af1126ee3
EttKon <74019344+EttKon@users.noreply.github.com>
2023-02-14 10:26:17 +0100

    Replaced ; to ,

--- assets/app.js
+++ assets/app.js
@@ -1,14 +1,14 @@
 const imageArray = [
-	"//EttKon.github.io/Pictures/3EBC40F4-C2B1-4F66-9727-2B2385822279.jpeg";
-	"//EttKon.github.io/Pictures/6834B942-62FC-47D1-BF9A-85342E8635DA.jpeg";
-	"//EttKon.github.io/Pictures/70273824-73E8-4053-8016-3BD1A6950865.jpeg";
-	"//EttKon.github.io/Pictures/A1643A56-B7A7-4747-AD3E-D7B7A287E73D.jpeg";
-	"//EttKon.github.io/Pictures/AA831393-05A7-408D-B252-9E4E4DB28A02.jpeg";
-	"//EttKon.github.io/Pictures/D703A266-5F69-454A-A360-38BCE21297EC.jpeg";
-	"//EttKon.github.io/Pictures/E10D0D27-46E7-431C-A5E8-74644E0FE7FC.jpeg";
-	"//EttKon.github.io/Pictures/E26962EB-764D-4409-A4EB-62B361B914A9.jpeg";
-	"//EttKon.github.io/Pictures/E5C61999-1D03-490A-9CF6-89E136038434.jpeg";
-	"//EttKon.github.io/Pictures/E917163F-A328-4115-9D52-0B35FF34B6A3.jpeg";
+	"//EttKon.github.io/Pictures/3EBC40F4-C2B1-4F66-9727-2B2385822279.jpeg",
+	"//EttKon.github.io/Pictures/6834B942-62FC-47D1-BF9A-85342E8635DA.jpeg",
+	"//EttKon.github.io/Pictures/70273824-73E8-4053-8016-3BD1A6950865.jpeg",
+	"//EttKon.github.io/Pictures/A1643A56-B7A7-4747-AD3E-D7B7A287E73D.jpeg",
+	"//EttKon.github.io/Pictures/AA831393-05A7-408D-B252-9E4E4DB28A02.jpeg",
+	"//EttKon.github.io/Pictures/D703A266-5F69-454A-A360-38BCE21297EC.jpeg",
+	"//EttKon.github.io/Pictures/E10D0D27-46E7-431C-A5E8-74644E0FE7FC.jpeg",
+	"//EttKon.github.io/Pictures/E26962EB-764D-4409-A4EB-62B361B914A9.jpeg",
+	"//EttKon.github.io/Pictures/E5C61999-1D03-490A-9CF6-89E136038434.jpeg",
+	"//EttKon.github.io/Pictures/E917163F-A328-4115-9D52-0B35FF34B6A3.jpeg"
 	/*,
 	"https://images.unsplash.com/photo-1495480393121-409eb65c7fbe?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=05ea43dbe96aba57d48b792c93752068&auto=format&fit=crop&w=1351&q=80",
 	"https://images.unsplash.com/photo-1501611724492-c09bebdba1ac?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=ebdb0480ffed49bd075fd85c54dd3317&auto=format&fit=crop&w=1491&q=80",
