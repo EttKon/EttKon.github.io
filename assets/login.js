1a49da8c8efe3c708add609935f15b737e65ccc5
EttKon <74019344+EttKon@users.noreply.github.com>
2023-06-29 16:20:41 +0200

    Added whole project

---
+++ assets/login.js
@@ -0,0 +1,19 @@
+let loginForm = document.getElementById("login-form");
+let loginButton = document.getElementById("login-btn");
+
+loginButton.addEventListener("click", (e) => {
+	e.preventDefault();
+	const pswd = loginForm.value;
+
+	if(pswd === "pickle") {
+		window.location.href = "/EttKon.github.io-Picture-Generator/picture_generator.html";
+	}
+	else {
+		alert("wrong password");
+	}
+});
+
+// loginButton.addEventListener("click", function(){
+// 	// document.location.href = "google.com";
+// 	alert("test")
+// })
